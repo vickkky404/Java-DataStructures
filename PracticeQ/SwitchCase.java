cec63a46e2eb2b7d75570b07833ab33fe2555347
nalin <nalinikantbastia@gmail.com>
2026-04-18 13:02:08 +0530

    Switch case

---
+++ PracticeQ/SwitchCase.java
@@ -0,0 +1,38 @@
+package PracticeQ;
+
+import java.util.Scanner;
+
+// Write a Java program that takes a day number (1–7) as input and prints the corresponding day name using a switch statement. If the number is outside the range 1–7, print "Invalid day".
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2;
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");   
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;  
+            default:
+                System.out.println("Invalid day");
+        }
+
+        sc.close();
+        
+    }
+    
+}
