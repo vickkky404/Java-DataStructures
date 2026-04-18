0e7c38c7f06161c8996511642166e9aebaa48588
nalin <nalinikantbastia@gmail.com>
2026-04-18 10:01:46 +0530

    Learning Loops and Condational

--- Basics/LoopsAndCondationals/Loops1.java
+++ Basics/LoopsAndCondationals/Loops1.java
@@ -1,5 +1,39 @@
 package Basics.LoopsAndCondationals;
 
+import java.util.Scanner;
+
+
 public class Loops1 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fruits = sc.next();
+
+        // if(fruits.equals("mango")){
+        //     System.out.println("King of Fruits.");
+        // }
+
+        // if(fruits.equals("apple")){
+        //     System.out.println("A sweet red fruit.");
+        // }
+
+        //  if(fruits.equals("orange")){
+        //     System.out.println("Round fruit.");
+        // }
+
+        //lets try switchcases
+        switch (fruits) {
+            case "mango":
+                System.out.println("King of Fruits.");
+                break;
+            case "apple":
+                System.out.println("A sweet red fruit.");
+                break;
+            case "orange":
+                System.out.println("Round fruit.");
+                break;
+            default:
+                System.out.println("Please enter a valid fruit name.");
+        }
+        
+    }
 }
