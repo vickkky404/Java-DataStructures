bff38b072ef941e01f4c22f53d1a1f1651448b62
nalin <nalinikantbastia@gmail.com>
2026-04-19 23:22:38 +0530

    Condationals

---
+++ Basics/LoopsAndCondationals/WhileLoop.java
@@ -0,0 +1,23 @@
+package Basics.LoopsAndCondationals;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+       /*
+       syntax:-
+
+
+         while(condition){
+          //body of loop
+         }
+
+       */
+
+         int num = 1;
+         while (num <= 10) {
+             System.out.println(num);
+             num++;
+         }
+
+         
+     }
+ }
