e9c4505ee941a5b4051bc8504e3f79088fa19a4c
nalin <nalinikantbastia@gmail.com>
2026-04-19 23:57:06 +0530

    character  Case check Program

--- PracticeQ/CaseCheck.java
+++ PracticeQ/CaseCheck.java
@@ -7,6 +7,13 @@ public class CaseCheck {
         Scanner sc = new Scanner(System.in);
 
         char ch = sc.next().trim().charAt(0);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Uppercase");
+        } else {
+            System.out.println("Not an alphabet");
+        }
     }
     
 }
