c4bacf03304c25c758bbf20410df3e133d14ffbe
nalin <nalinikantbastia@gmail.com>
2026-04-19 23:25:36 +0530

    Do while loop

---
+++ Basics/DoWhile.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        /*
+        syntax:-
+            do {
+                // code to be executed
+            } while (condition);
+        */
+
+            int num = 1;
+            do{
+                System.out.println(num);
+                num++;
+            } while (num <= 5);
+
+    }
+}
